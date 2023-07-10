6ecbceb806bc295c9eaa359abc28928e38990510
AlejandroMartinez19 <138024953+AlejandroMartinez19@users.noreply.github.com>
2023-07-10 10:15:38 -0500

    Add files via upload

---
+++ EXAMEN DE PROGRAMACION.py
@@ -0,0 +1,166 @@
+def menu():
+    while True:
+        print("Bienvenido al menú de importación de programas.")
+        print("1. Importar Programa 1")
+        print("2. Importar Programa 2")
+        print("3. Importar Programa 3")
+        print("4. Importar Programa 4")
+        print("5. Programa que calcula el perimetro de un rectangulo")
+        print("7. Programa que calcula el volumen de un prisma rectangular")
+        print("8. Programa que resuelve ecuaciones1")
+        print("9. Programa que resuelve ecuaciones1.1")
+        print("10. Programa que transforma de metros a pies y a pulgadas")
+        print("11. Programa que resuelve una regla de tres")
+        print("12. Programa que calcula el interes de un prestamo")
+        print("13. Programa que calcula el salario neto")
+        print("14. Programa que calcula el costo de la gasolina")
+        print("15. Programa que  calcula el costo de tres articulos")
+        print("16. Programa que calula la nota")
+        print("17. Programa que calcula el costo de cinco articulos")
+        print("18. Programa que calcula si paga impuestos")
+        print("19. Programa que calcula cuanto es el impuesto")
+        print("20. Programa que indica la temperatura")
+        print("21. Programa que indica un número del 6 a 18")
+        print("22. Programa que indica el mayor de 3 numeros")
+        print("23. Programa que calcula descuentos")
+        print("24. Programa que clasifica triangulos")
+        print("25. Programa que indica si es mayor, menor o cero")
+        print("26. Programa que me dice la calificación")
+        print("27. Programa que me dice calificaciones")
+        print("28. Programa que clasifica 3 numeros")
+        print("29. Programa que me indica siete numeros")
+        print("30. Programa que me dice seis numeros")
+        print("31. Programa que genera las tablas de multiplicar")
+        print("32. Programa que indica si es par o impar")
+        print("33. Programa que clasifica numeros")
+        print("34. Programa que calcula el impuesto de cinco articulos")
+        print("35. Programa que indica el mayor de tres")
+        print("36. Programa que suma numeros pares")
+        print("37. Programa que calcula el area de un triangulo")
+        print("38. Programa que clasifica numeros")
+        print("39. Salir")
+
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            import programa1
+            print("Programa 1 importado correctamente.")
+        elif opcion == "2":
+            import programa2
+            print("Programa 2 importado correctamente.")
+        elif opcion == "3":
+            import programa3
+            print("Programa 3 importado correctamente.")
+        elif opcion == "4":
+            import programa4
+            print("Programa 4 importado correctamente.")
+        elif opcion == "5":
+            import programa5
+            print("Programa 5 importado correctamente.")
+        elif opcion == "6":
+            import programa6
+            print("Programa 6 importado correctamente.")
+        elif opcion == "7":
+            import programa7
+            print("Programa 7 importado correctamente.")
+        elif opcion == "8":
+            import Programa8
+            print("Programa 8 importado correctamente.")
+        elif opcion == "9":
+            import Programa9
+            print("Programa 9 importado correctamente.")
+        elif opcion == "10":
+            import Programa10
+            print("Programa 10 importado correctamente.")
+        elif opcion == "11":
+            import Programa11
+            print("Programa 11 importado correctamente.")
+        elif opcion == "12":
+            import Programa12
+            print("Programa 12 importado correctamente.")
+        elif opcion == "13":
+            import Programa13
+            print("Programa 13 importado correctamente.")
+        elif opcion == "14":
+            import Programa14
+            print("Programa 14 importado correctamente.")
+        elif opcion == "15":
+            import Programa15
+            print("Programa 15 importado correctamente.")
+        elif opcion == "16":
+            import Programa16
+            print("Programa 16 importado correctamente.")
+        elif opcion == "17":
+            import Programa17
+            print("Programa 17 importado correctamente.")
+        elif opcion == "18":
+            import Programa18
+            print("Programa 18 importado correctamente.")
+        elif opcion == "19":
+            import Programa19
+            print("Programa 19 importado correctamente.")
+        elif opcion == "20":
+            import Programa20
+            print("Programa 20 importado correctamente.")
+        elif opcion == "21":
+            import Programa21
+            print("Programa 21 importado correctamente.")
+        elif opcion == "22":
+            import Programa22
+            print("Programa 22 importado correctamente.")
+        elif opcion == "23":
+            import Programa23
+            print("Programa 23 importado correctamente.")
+        elif opcion == "24":
+            import Programa24
+            print("Programa 24 importado correctamente.")
+        elif opcion == "25":
+            import Programa25
+            print("Programa 25 importado correctamente.")
+        elif opcion == "26":
+            import Programa26
+            print("Programa 26 importado correctamente.")
+        elif opcion == "27":
+            import Programa27
+            print("Programa 27 importado correctamente.")
+        elif opcion == "28":
+            import Programa28
+            print("Programa 28 importado correctamente.")
+        elif opcion == "29":
+            import Programa29
+            print("Programa 29 importado correctamente.")
+        elif opcion == "30":
+            import Programa30
+            print("Programa 30 importado correctamente.")
+        elif opcion == "31":
+            import Programa31
+            print("Programa 31 importado correctamente.")
+        elif opcion == "32":
+            import Programa32
+            print("Programa 32 importado correctamente.")
+        elif opcion == "33":
+            import Programa33
+            print("Programa 33 importado correctamente.")
+        elif opcion == "34":
+            import Programa34
+            print("Programa 34 importado correctamente.")
+        elif opcion == "35":
+            import Programa35
+            print("Programa 35 importado correctamente.")
+        elif opcion == "36":
+            import Programa36
+            print("Programa 36 importado correctamente.")
+        elif opcion == "37":
+            import Programa37
+            print("Programa 37 importado correctamente.")
+        elif opcion == "38":
+            import Programa38
+            print("Programa 38 importado correctamente.")
+        elif opcion == "39":
+            print("Saliendo del menú...")
+            break
+        else:
+            print("Opción inválida. Por favor, seleccione una opción válida.")
+
+menu()
+
